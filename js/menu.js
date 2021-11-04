e133db158b514596a23de21a88f20f5dbe691d02
KhrystynaHirna <86721411+KhrystynaHirna@users.noreply.github.com>
2021-11-04 14:45:40 +0100

    add

--- js/menu.js
+++ js/menu.js
@@ -14,12 +14,12 @@
 // })();
 
 (() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const openMenuBtn = document.querySelector('.is-open');
-  const closeMenuBtn = document.querySelector('.is-close');
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
 
   const toggleMenu = () => {
-  const isMenuOpen =
+    const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
